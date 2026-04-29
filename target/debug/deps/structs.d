ea4b8325117a365f8dfdea2eb2c4000113f1647c
Tushar Kumar <tusharkumar1036@gmail.com>
2026-04-30 03:19:30 +0530

    structs work

---
+++ target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+C:\Users\tusha\OneDrive\Desktop\Rust\structs\target\debug\deps\structs.d: src\main.rs
+
+C:\Users\tusha\OneDrive\Desktop\Rust\structs\target\debug\deps\structs.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\tusha\OneDrive\Desktop\Rust\structs\target\debug\structs.exe: C:\Users\tusha\OneDrive\Desktop\Rust\structs\src\main.rs
