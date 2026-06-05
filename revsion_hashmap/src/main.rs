6688b42ee84a33a261d9e111e8571df93859bf97
Tushar Kumar <tusharkumar1036@gmail.com>
2026-06-05 13:04:02 +0530

    hashmap

---
+++ revsion_hashmap/src/main.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;//using hashmap library
+
+fn main() {
+    let mut map = HashMap::new();//creating new hash
+
+    map.insert("a", 1);//inserting the key value
+    map.insert("b",3);
+
+    if let Some(v) = map.get("b") {
+        println!("{}", v);//it will print the value 
+    }
+     // Loop through all key-value pairs
+    for (key, value) in &map {
+        println!("{} => {}", key, value);
+    }
+}
