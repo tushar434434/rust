a521e95fd6455fc3ab14f1776ff3fd5bec427cc6
Tushar Kumar <tusharkumar1036@gmail.com>
2026-06-25 22:15:06 +0530

    handling the thred completion

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,4 +1,5 @@
 //==========Creating a thread======
+/*
 use std::thread;
 use std::time::Duration;
 fn main(){
@@ -12,4 +13,22 @@ fn main(){
         println!("hi number {i} is from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+}*/
+//the output will be ki main thread jb tk chlega tbhi tk spawn thread chlega
+//A JoinHandle<T> is an owned value that, when we call the join method on it, will wait for its thread to finish. 
+
+use std::thread;
+use std::time::Duration;
+fn main(){
+    let handle =thread::spawn(||{
+        for i in 1..10 {
+            println!("this number {i} is from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5 {
+        println!("this number {i} is from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
 }
