0ea0310472afba58d4fe5a8780b7ea8f93a87691
Tushar Kumar <tusharkumar1036@gmail.com>
2026-06-25 22:31:37 +0530

    move

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -16,7 +16,7 @@ fn main(){
 }*/
 //the output will be ki main thread jb tk chlega tbhi tk spawn thread chlega
 //A JoinHandle<T> is an owned value that, when we call the join method on it, will wait for its thread to finish. 
-
+/*
 use std::thread;
 use std::time::Duration;
 fn main(){
@@ -26,9 +26,29 @@ fn main(){
             thread::sleep(Duration::from_millis(1));
         }
     });
+   // handle.join().unwrap();  // the main thread will wait for the spawned thread to complete
     for i in 1..5 {
         println!("this number {i} is from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+   handle.join().unwrap();
+}*/
+//using move closures with thread
+/*
+use std::thread;
+fn main(){
+    let v = vec![1,2,3];
+    let handle = thread::spawn(||{
+        println!("here is a vector: {v:?}");
+    });
+    handle.join().unwrap();
+}
+*/
+use std::thread;
+fn main(){
+    let v = vec![1,2,3];
+    let handle = thread::spawn(move||{//it will tale ownership of the value
+        println!("here is a vector: {v:?}");
+    });
     handle.join().unwrap();
 }
