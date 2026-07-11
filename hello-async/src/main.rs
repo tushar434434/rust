6cda468204aefb1451b51f2fa4800748a8713e38
Tushar Kumar <tusharkumar1036@gmail.com>
2026-07-11 16:08:54 +0530

    join!

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -105,7 +105,7 @@ fn main(){
         trpl::sleep(Duration::from_millis(500)).await;
     }
     handle.await.unwrap();
-*/
+*//*
         let fut1 = async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
@@ -121,6 +121,98 @@ fn main(){
         };
 
         trpl::join(fut1, fut2).await;
+        */
+
+
+            //=======Sending data btw two task using message passing=====
+            /*
+            let (tx,mut rx) = trpl::channel();
+            let val = String::from("hi");
+            tx.send(val).unwrap();
+            let received = rx.recv().await.unwrap();
+            println!("Got: {received}");
+            */
+            
+            let (tx, mut rx) = trpl::channel();
+            /*
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("other"),
+                String::from("side"),
+            ];
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+            while let Some(value) = rx.recv().await {
+                println!("Got: {value}");
+            }
+*/
+/*
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
+*/
+//======= joining a number of futures with the join! macro
+
+       // let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join!(tx1_fut, tx_fut, rx_fut);
 
 }
 );
