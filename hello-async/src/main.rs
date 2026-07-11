3b99fc708263072f13a74a2b92ba40d1364c75a7
Tushar Kumar <tusharkumar1036@gmail.com>
2026-07-11 15:16:58 +0530

    spawn_task

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -42,10 +42,11 @@ fn main() {
     })
 }*/
 //Racing Two URLs Against Each Other Concurrently
+/*
 use trpl::{Either, Html};
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    trpl::block_on(async {
+    trpl::block_on(async {//trpl::block_on is a function that takes an asynchronous block of code and runs it to completion. It blocks the current thread until the asynchronous code has finished executing, allowing us to use async/await syntax in a synchronous context.
         let title_fut_1 = page_title(&args[1]);
         let title_fut_2 = page_title(&args[2]);
         let (url, maybe_title) =
@@ -67,5 +68,65 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
         .map(|title| title.inner_html());
     (url, title)
 }
+*/
+
+
+//=========applying concurrency with async ==============
+
+//creating  a new task with spawn_task
+use std::time::Duration;
+
+fn main(){
+    trpl::block_on(async{
+        /*async {
+        trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        }
+        ) ;
+        for i in 1..5{
+            
+            println!("hi number {i} from the main task!");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }   
+     }
+        */
+        /*
+    let handle =trpl::spawn_task(async {
+        for i in 1..10 {
+            println!("hi number {i} from the first task!");
+            trpl::sleep(Duration::from_millis(500)).await;  
+        }
+    });
+    for i in 1..5{
+        println!("hi number {i} from the main task!");
+        trpl::sleep(Duration::from_millis(500)).await;
+    }
+    handle.await.unwrap();
+*/
+        let fut1 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
+
+}
+);
+
+}
+
+
 
 
